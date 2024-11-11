a83bb5fac527c55ae8d5df8e974eb5629275b31a
Valdson Silva Cleto <vcleto@gmail.com>
2024-11-11 15:59:41 -0300

    Criação da pasta para o projeto da Publicação 2023, a partir da pasta "Publicação 2022 livro HTML"

---
+++ Publicação 2023/_setup.R
@@ -0,0 +1,39 @@
+gera.graficos.office = FALSE;
+
+lista.de.pacotes = c("rmarkdown",
+                     "tinytex",
+
+# Substituído o conjunto de pacotes tidyverse pelos pacotes separadamente porque não funciona o carregamento do tidyverse no R do Ministério
+#                     "tidyverse",
+                     "dplyr",
+                     "purrr",
+                     "tidyr",
+                     "readr",
+                     "stringr",
+                     "forcats",
+
+                     "janitor",
+                     "repmis",
+                     "ggrepel",
+                     "renv",
+                     "readODS",
+                     "prismatic") # escreva a lista de pacotes
+
+if(gera.graficos.office) {
+  lista.de.pacotes <- append(lista.de.pacotes, "export")
+  arquivo_graficos = "Censo-SUAS-2022-graficos"
+  arquivo_graficos_criado = FALSE
+}
+
+novos.pacotes <- lista.de.pacotes[!(lista.de.pacotes %in%
+                                      installed.packages()[,"Package"])]
+
+if(length(novos.pacotes) > 0) {install.packages(novos.pacotes, repos = "http://cran.us.r-project.org")}
+
+# adiciona pacotes do tidyverse para serem carregados
+lista.de.pacotes <- append(lista.de.pacotes, c("readxl", "scales"))
+
+lapply(lista.de.pacotes, require, character.only=T)
+rm(lista.de.pacotes,novos.pacotes)
+gc()
+
