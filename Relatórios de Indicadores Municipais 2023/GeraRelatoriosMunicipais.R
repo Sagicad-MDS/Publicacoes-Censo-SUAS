c7f2ea97936fb99fc58175ac4c8f03794e5a6c83
vcleto <vcleto@gmail.com>
2025-04-18 11:42:37 -0300

    Colocado novamente o carregamento de pacotes no script R de geração dos relatórios de cada município, porque não estava funcionando o carregamento no arquivo fonte do relatório.

--- Relatórios de Indicadores Municipais 2023/GeraRelatoriosMunicipais.R
+++ Relatórios de Indicadores Municipais 2023/GeraRelatoriosMunicipais.R
@@ -1,3 +1,15 @@
+lista.de.pacotes = c("tinytex","tidyr", "purrr", "dplyr",
+                     "lubridate","janitor","readxl","readr","readODS","stringr",
+                     "repmis","scales",
+                     "ggrepel","renv","knitr","quarto") # escreva a lista de pacotes
+
+novos.pacotes <- lista.de.pacotes[!(lista.de.pacotes %in%
+                                      installed.packages()[,"Package"])]
+
+if(length(novos.pacotes) > 0) {install.packages(novos.pacotes, repos = "http://cran.us.r-project.org")}
+lapply(lista.de.pacotes, require, character.only=T)
+rm(lista.de.pacotes,novos.pacotes)
+
 setwd("/home/valdio/Publicacoes-Censo-SUAS/Relatórios de Indicadores Municipais 2023")
 
 dados_municipios <- read_excel("../RELATORIO_DTB_BRASIL_MUNICIPIO.xls", skip = 6) %>%
