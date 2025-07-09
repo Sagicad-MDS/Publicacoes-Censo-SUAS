fb9769f761a88d7f859596c5dea6f9b3fbee79bf
vcleto <vcleto@gmail.com>
2025-07-09 12:34:39 -0300

    Remove o código que sempre verificava se precisava instalar algum pacote, porque essa automatização nao ajuda muito e não funciona direito. Faz o carregamento direto dos pacotes necessários em vez de utilizar a função lapply a uma lista de pacotes, para que funções que controlam o ambiente de pacotes utilizados funcionem.

--- Publicação 2023/_setup.R
+++ Publicação 2023/_setup.R
@@ -1,39 +1,27 @@
-gera.graficos.office = FALSE;
-
-lista.de.pacotes = c("rmarkdown",
-                     "tinytex",
-
+library("rmarkdown")
+library("tinytex")
 # Substituído o conjunto de pacotes tidyverse pelos pacotes separadamente porque não funciona o carregamento do tidyverse no R do Ministério
-#                     "tidyverse",
-                     "dplyr",
-                     "purrr",
-                     "tidyr",
-                     "readr",
-                     "stringr",
-                     "forcats",
-                     "readxl",
-                     "janitor",
-                     "repmis",
-                     "ggrepel",
-                     "renv",
-                     "readODS",
-                     "prismatic") # escreva a lista de pacotes
+#library("tidyverse")
+library("dplyr")
+library("purrr")
+library("tidyr")
+library("readr")
+library("stringr")
+library("forcats")
+library("readxl")
+library("janitor")
+library("repmis")
+library("ggrepel")
+library("renv")
+library("readODS")
+library("prismatic")
+library("scales")
 
+gera.graficos.office = FALSE;
 if(gera.graficos.office) {
-  lista.de.pacotes <- append(lista.de.pacotes, "export")
-  arquivo_graficos = "Censo-SUAS-2022-graficos"
+  library("export")
+  arquivo_graficos = "Censo-SUAS-2023-graficos"
   arquivo_graficos_criado = FALSE
 }
 
-novos.pacotes <- lista.de.pacotes[!(lista.de.pacotes %in%
-                                      installed.packages()[,"Package"])]
-
-if(length(novos.pacotes) > 0) {install.packages(novos.pacotes, repos = "http://cran.us.r-project.org")}
-
-# adiciona pacotes do tidyverse para serem carregados
-lista.de.pacotes <- append(lista.de.pacotes, c("scales"))
-
-lapply(lista.de.pacotes, require, character.only=T)
-rm(lista.de.pacotes,novos.pacotes)
-gc()
 
